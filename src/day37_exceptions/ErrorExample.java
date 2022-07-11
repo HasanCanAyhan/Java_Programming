2a2b96a5ee357b61527de796f33d9726855e70ae
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-07-11 23:22:22 +0200

    Polymorphism

---
+++ src/day37_exceptions/ErrorExample.java
@@ -0,0 +1,30 @@
+package day37_exceptions;
+
+import java.util.ArrayList;
+
+public class ErrorExample {
+
+    public static void main(String[] args) {
+
+        // error can not be recovered!!!-irrecoverable
+
+        /*
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+
+        while (true){//infinite loop --->>> OutOfMemoryError : Java heap space : Issue of system resource
+            pizzas.add( new Pizza('S',2,3)  );
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+    }
+
+}
